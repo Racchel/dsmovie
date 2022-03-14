5eb4faa3484d9da4a311291d4023b3057f59ffb6
Racchel <racchelvelasco@gmail.com>
2022-03-14 00:41:31 -0300

    [FRONT] useState and useEffect 🚀

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Pagination } from 'components'
 import { Card as MovieCard } from 'components/Movie'
 import { BASE_URL } from 'utils/requests'
+import { MoviePage } from 'types/movie'
 
 const Listing:React.FC = () => {
 
-  // FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=1`)
-    .then(response => {
-      console.log(response.data)
-    })
+  const [pageNumber, setPageNumber] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+      .then(response => {
+        const data = response.data as MoviePage
+        console.log(data)
+        setPageNumber(data.number)
+      })
+  }, [])
 
   const arr = Array.from({ length: 10 })
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className='container'>
         <div className='row'>
